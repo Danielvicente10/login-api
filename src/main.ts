c768e099b25704ff83c2560cbda889c7b8d14266
Daniel Vicente <danielvicente63@gmail.com>
2024-06-02 22:22:07 -0300

    correção para comunicação do front com a api

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS using NestJS's built-in method
+  app.enableCors({
+    origin: "http://localhost:3000", // Your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(3030);
 }
 bootstrap();
